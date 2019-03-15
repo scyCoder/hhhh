426dcfc18c583e37c3f1ac11870c057b098ebb9f
scy <120@qq.com>
2019-03-15 14:50:19 +0800

    hhhh

---
+++ src/main/java/com/jrx/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.jrx.demo;
+
+/**
+ * @Author: sunchuanyin
+ * @Date: 2019/3/15 13:30
+ * @Version 1.0
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
